46c2ad8cda70e778622c225237d9c8ebdd77d54e
camille225 <makaye151@gmail.com>
2018-11-20 06:39:40 +0000

    Deployement du projet de Adz

---
+++ adz/src/app/adz.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AdzComponent } from './adz.component';
+
+@NgModule({
+  declarations: [
+    AdzComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AdzComponent]
+})
+export class AdzModule { }
