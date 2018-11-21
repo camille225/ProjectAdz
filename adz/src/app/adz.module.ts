2f6247da867829ba42df26ee9eb1df6f1ed6030a
camille225 <makaye151@gmail.com>
2018-11-21 07:20:20 +0000

    Ajout de matérial-angular au projet et creation d'un composant pour la recherche des books

--- adz/src/app/adz.module.ts
+++ adz/src/app/adz.module.ts
@@ -1,14 +1,19 @@
+
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
 import { AdzComponent } from './adz.component';
-
+import {BookSearchComponent} from './book-search/book-search.component';
+import { MaterialAngularModule } from 'src/material-angular/material-angular.module';
 @NgModule({
   declarations: [
-    AdzComponent
+    AdzComponent,
+    BookSearchComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialAngularModule,
   ],
   providers: [],
   bootstrap: [AdzComponent]
